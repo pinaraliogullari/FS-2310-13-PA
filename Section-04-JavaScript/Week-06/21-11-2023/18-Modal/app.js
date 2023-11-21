9cbcfe3da8ed2dbb6e191d76407e19cbcabb480e
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2023-11-21 21:56:56 +0300

    Js Modal app ok

---
+++ Section-04-JavaScript/Week-06/21-11-2023/18-Modal/app.js
@@ -0,0 +1,33 @@
+const btnOpenModal = document.getElementById("btn-open-modal"); // birden fazla kez kullanma ihtimalimiz  olduğu için bir değişkene atıyoruz.
+const mainModal = document.querySelector(".main-modal");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnNoModal = document.querySelector(".btn-no-modal");
+
+btnOpenModal.addEventListener("click", function () {
+    mainModal.classList.remove("main-modal-none");
+});
+
+//btnCloseModal ı ve btnNoModalı  da yukarıaki gibi yazabilirdik. Ancak fonklar aynı olduğu için bu şekilde yaptık.
+btnCloseModal.addEventListener("click", closeModal);
+btnNoModal.addEventListener("click", closeModal);
+mainModal.addEventListener("click", function(event){
+    console.log(event.target);//target click nerede gerçekleşiyorsa onu veriyor. console dan takip edebiliriz.
+    //kutunun dıiına tıklanınca da kapansın istiyoruz.
+    //tek bir class tanımlıysa bunu kullanabiliriz.
+// if(event.target.className=="main-modal"){
+//     closeModal();
+// }
+
+if(event.target.classList.contains("main-modal")){
+    closeModal();
+};
+  
+});
+
+
+
+
+
+function closeModal(){
+    mainModal.classList.add("main-modal-none");
+};
