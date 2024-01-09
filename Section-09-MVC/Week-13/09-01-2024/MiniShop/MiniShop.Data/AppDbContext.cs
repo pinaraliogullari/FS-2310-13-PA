bb5dfe54683769fba67ba6e38fea8ad181aa5b47
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-01-09 21:53:04 +0300

    profuct data has config continue

---
+++ Section-09-MVC/Week-13/09-01-2024/MiniShop/MiniShop.Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniShop.Data.Config;
+using MiniShop.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CategoryProduct> CategoryProducts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-QE6JDF1\SQLEXPRESS;Database=MiniShopDb;User Id=sa;Password=pe969416; TrustServerCertificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.ApplyConfiguration(new CategoryConfig());
+          
+
+          
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
