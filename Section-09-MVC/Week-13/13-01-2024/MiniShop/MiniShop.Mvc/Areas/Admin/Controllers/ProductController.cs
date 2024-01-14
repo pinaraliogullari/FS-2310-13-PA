8c3a29dba71190090c93b163bbe7af450129719c
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-01-14 14:52:54 +0300

    api is started

---
+++ Section-09-MVC/Week-13/13-01-2024/MiniShop/MiniShop.Mvc/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+
+namespace MiniShop.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productManager;
+
+        //Dependency Injection
+        public ProductController(IProductService productManager)
+        {
+            _productManager = productManager;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productManager.GetAll();
+            return View(products);
+        }
+    }
+}
---
+++ Section-09-MVC/Week-13/14-01-2024/MiniShop/MiniShop.Mvc/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+using MiniShop.Core.ViewModels;
+
+namespace MiniShop.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productManager;
+
+        //Dependency Injection
+        public ProductController(IProductService productManager)
+        {
+            _productManager = productManager;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productManager.GetAll(null, null, false);
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //ekrana kullanıcının yeni ürün girmesi için gerekli sayfayı açacak. kayıt yapmayacak.
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ProductViewModel productViewModel)
+        {
+       
+           _productManager.Create(productViewModel);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ProductViewModel editedProduct = _productManager.GetById(id);
+            return View(editedProduct);
+        }
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel editProduct)
+        {
+            _productManager.Update(editProduct);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            ProductViewModel deletedProduct = _productManager.GetById(id);
+             return View(deletedProduct);
+        }
+
+
+        public IActionResult HardDelete(int id)
+        {
+            _productManager.HardDelete(id);
+            return RedirectToAction("Index");
+        }
+        public IActionResult SoftDelete(int id)
+        {
+            _productManager.SoftDelete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
