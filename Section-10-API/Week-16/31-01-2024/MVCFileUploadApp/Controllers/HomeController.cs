a3a1abeea8fdf330e562369e09d6845d9eb60d8c
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-01-31 21:50:49 +0300

    image upload

---
+++ Section-10-API/Week-16/31-01-2024/MVCFileUploadApp/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFileUploadApp.Core;
+using MVCFileUploadApp.Models;
+using System.Diagnostics;
+
+namespace MVCFileUploadApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //herhangibir dosya karşılayacaksak kullanacağımız tip IFormFile
+        public IActionResult UploadImage(IFormFile image)
+        {
+            if (image == null)
+            {
+                ViewBag.Message = "Resimde bir sorun var. Lütfen tekrar deneyiniz.";
+                return View("Index");
+            }
+            //localhost:5000/wwwroot/images/product5.png
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", image.FileName);
+            using (var stream=new FileStream(path,FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+                ViewBag.Message = "Resim başarıyla yüklendi";
+            return View("Index");
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ResimYukle()
+        {
+         
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ResimYukle(IFormFile image)
+        {
+            var imageUrl = Uploader.UploadImage(image);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
