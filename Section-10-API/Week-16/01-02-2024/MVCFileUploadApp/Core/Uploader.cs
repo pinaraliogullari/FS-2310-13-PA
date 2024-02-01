5f44b93c5ca5500a7ab11301b95dc659fd38c780
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-02-01 21:54:46 +0300

    image upload process on mvc

---
+++ Section-10-API/Week-16/01-02-2024/MVCFileUploadApp/Core/Uploader.cs
@@ -0,0 +1,30 @@
+﻿namespace MVCFileUploadApp.Core
+{
+    public class Uploader
+    {
+        public static string UploadImage(IFormFile image)
+        {
+            //dosyaismi.uzantı
+            //manzara.kadikoy.png=[manzara, kadikoy,png]
+            //Örnek image ismi: iphone12.png
+
+
+            var extension="." + image.FileName.Split('.')[image.FileName.Split('.').Length-1];// sonuc olarak .png verecek.
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");//localhost:5000/wwwroot/images
+            if(!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+           
+            var fileName=$"{Guid.NewGuid().ToString()} {extension}";  //hasgdasdjgj.png
+            var path=Path.Combine(filePath, fileName); //localhost:5000/wwwroot/images/hasgdasdjgj.png
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
