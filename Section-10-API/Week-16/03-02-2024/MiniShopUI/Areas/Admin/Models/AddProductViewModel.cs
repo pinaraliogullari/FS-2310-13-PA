7667bf675873651da97e4f63d0943eacf84c3313
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-02-03 22:26:24 +0300

    mvc image edit is ok

---
+++ Section-10-API/Week-16/03-02-2024/MiniShopUI/Areas/Admin/Models/AddProductViewModel.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MiniShop.MVC.Areas.Admin.Models
+{
+    public class AddProductViewModel
+    {
+    
+        //buradakileri jsona çevirip gönderdiðimiz için IFormFile image propunu buraya eklemiyoruz.Create actionuna post olarak ekliyoruz.
+
+        [JsonPropertyName("Name")]
+        [DisplayName("Ürün")] //bunu yapma sebebimiz merkezi bir yerden isimleri kontrol etmek istememiz. bir çok yerde varsa her yerden deðiþtirmemize gerek kalmaz. 
+        [Required(ErrorMessage = "{0} alaný boþ býrakýlamaz.")]   //validation için mvcnin bize sunduðu data annotaiýný kullanacaðýz. baþka yollarý da var.
+        [MinLength(5,ErrorMessage ="{0} alanýna uzunluðu {1} karakterden küçük deðer girilemez.")]
+
+
+        public string Name { get; set; }
+
+
+        [JsonPropertyName("Properties")]
+        [DisplayName("Ürün Özellikleri")]
+        public string Properties { get; set; }
+
+        
+        [JsonPropertyName("Price")]
+        [DisplayName("Ürün Fiyatý")]
+        [Required(ErrorMessage = "{0} alaný boþ býrakýlamaz.")]
+        public decimal? Price { get; set; }
+
+
+        [JsonPropertyName("ImageUrl")]
+        [DisplayName("Resim")]
+        public string ImageUrl { get; set; } = "";
+
+
+        [JsonPropertyName("Url")]
+        public string Url { get; set; }
+
+        
+        [JsonPropertyName("IsActive")]
+        [DisplayName("Aktif Ürün")]
+        public bool IsActive { get; set; }
+
+        
+        [JsonPropertyName("IsHome")]
+        [DisplayName("Ana Sayfa Ürünü")]
+        public bool IsHome { get; set; }
+
+
+        [JsonPropertyName("CategoryIds")]
+        public List<int> CategoryIds { get; set; }= new List<int>();
+
+
+        [DisplayName("Kategoriler")]
+        public List<CategoryViewModel> Categories { get; set; }
+    }
+}
---
+++ Section-10-API/Week-16/03-02-2024/MiniShopUI/Areas/Admin/Models/DeleteProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MiniShop.MVC.Areas.Admin.Models
+{
+    public class DeleteProductViewModel
+    {
+        //burada da display name kullanabilir miyiz
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
