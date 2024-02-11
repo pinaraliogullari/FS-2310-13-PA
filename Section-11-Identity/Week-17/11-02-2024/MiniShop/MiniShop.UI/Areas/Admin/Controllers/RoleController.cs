104e743407ec2825dc776bb00c143bcdca10e1b7
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-02-11 15:00:40 +0300

    role assign is ok

---
+++ Section-11-Identity/Week-17/11-02-2024/MiniShop/MiniShop.UI/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniShop.Entity.Concrete.Identity;
+
+namespace MiniShop.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "SuperAdmin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<Role> _roleManager;
+        private readonly UserManager<User> _userManager;
+
+
+        public RoleController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index() => View(await _roleManager.Roles.ToListAsync());
+
+
+    }
+}
