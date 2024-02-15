a3b325c542d3165017aff5e83f13103d19c4f375
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-02-15 22:10:47 +0300

    displaying the quantities on the  basket is ok

---
+++ Section-11-Identity/Week-18/15-02-2024/MiniShop/MiniShop.Business/Concrete/ShoppingCartItemManager.cs
@@ -0,0 +1,34 @@
+﻿using MiniShop.Business.Abstract;
+using MiniShop.Data.Abstract;
+using MiniShop.Entity.Concrete;
+using MiniShop.Shared.ResponseViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Business.Concrete
+{
+    public class ShoppingCartItemManager : IShoppingCartItemService
+    {
+        private readonly IShoppingCartItemRepository _shoppingCartItemRepository;
+
+        public ShoppingCartItemManager(IShoppingCartItemRepository shoppingCartItemRepository)
+        {
+            _shoppingCartItemRepository = shoppingCartItemRepository;
+        }
+
+        public async Task<Response<NoContent>> ChangeQuantityAsync(int shoppingCartItemId, int quantity)
+        {
+            ShoppingCartItem shoppingCartItem = await _shoppingCartItemRepository.GetByIdAsync(x=>x.Id==shoppingCartItemId);
+            await _shoppingCartItemRepository.ChangeQuantityAsync(shoppingCartItem, quantity);
+            return Response<NoContent>.Success();
+        }
+
+        public async Task<int> CountAsync(int shoppingCartId)
+        {
+            return await _shoppingCartItemRepository.GetCount(x=>x.ShoppingCartId==shoppingCartId);
+        }
+    }
+}
