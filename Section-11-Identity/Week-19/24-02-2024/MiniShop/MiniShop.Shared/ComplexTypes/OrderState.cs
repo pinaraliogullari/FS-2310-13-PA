1a738aa4dc6a792c21d5fc375b2abe98bd6f81a6
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-02-24 14:50:26 +0300

    order filter process  started

---
+++ Section-11-Identity/Week-19/24-02-2024/MiniShop/MiniShop.Shared/ComplexTypes/OrderState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ComplexTypes
+{
+    public enum OrderState
+    {
+        [Display(Name ="Sipariş alındı")]
+         Received=0,
+
+        [Display(Name = "Hazırlanıyor")]
+        Preparing =1,
+
+        [Display(Name = "Gönderildi")]
+        Sent =2,
+
+        [Display(Name = "Teslim edildi")]
+        Delivered =3
+
+    }
+}
---
+++ Section-11-Identity/Week-19/24-02-2024/MiniShop/MiniShop.Shared/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MiniShop.Shared.Extensions
+{
+    public static class EnumExtensions
+    {
+        //enum ın sistem sınıfını genişletiyoruz. var olan özelliklerine yeni bir özellik ekliyoruz.
+        //tüm enumlar bu metodu kullanabilir.
+        
+        public static string GetDisplayName(this Enum value)
+        {
+            string result= value
+                .GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                ?.GetName();
+            return result;
+        }
+    }
+}
